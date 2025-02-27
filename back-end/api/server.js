38b4bb5d572232a7eff70a38c8514caf767527e4
Avellax Avellax <avellac74@gmail.com>
2025-02-27 11:40:30 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -6,22 +6,33 @@
 import express from "express";
 import cors from "cors";  // Middleware (função que fica no meio, entre o pedido de requisição e o destino)
 import { db } from "./connect.js";
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get('/', (request, response) => {
-    response.send("Só vamos trabalhar com os endpoints '/artists' o '/songs'!");
+app.get('/api/', (request, response) => {
+    response.send("Só vamos trabalhar com os endpoints '/api/artists' o '/api/songs'!");
 })
 
-app.get('/artists', async (request, response) => {
+app.get('/api/artists', async (request, response) => {
     response.send(await db.collection('artists').find({}).toArray());
 })
 
-app.get('/songs', async (request, response) => {
+app.get('/api/songs', async (request, response) => {
     response.send(await db.collection('songs').find({}).toArray());
+})
+
+// configura este caminho para ser acessado pela URL 
+// (note que o "serve.js" está no back-end e o arquivo que queremos abrir está no front-end)
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+// qualquer coisa diferente dos caminhos anteriores, caem neste arquivo
+app.get('*', async (request, response) => {
+    response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 })
 
 app.listen(PORT, () => {
